f2c591f88c83cdba4f1a30e890da5156acc7c177
Peau Clement <clement.peau@epitech.eu>
2016-01-17 23:03:51 +0000

    finnit

--- get_next_line.c
+++ get_next_line.c
@@ -1,91 +1,92 @@
 /*
-** get_next_line.c for getnextline in /home/peau_c/rendu/CPE/GNL/CPE_2015_getne
+** get_next_line.c for gnlc in /home/peau_c/rendu/CPE/GNL/CPE_2015_getnextline
 **
 ** Made by
 ** Login   <peau_c@epitech.net>
-m**
-** Started on  Thu Jan 14 19:21:38 2016
-** Last update Sun Jan 17 15:43:46 2016 Clement Peau
+**
+** Started on  Tue Dec 29 13:19:17 2015
+** Last update Sun Jan 17 23:03:21 2016 Clement Peau
 */
 
 #include "get_next_line.h"
 
-char	*my_strcat(char *dest, char *src, int l)
+int			my_strncat(char *dest, char *src)
 {
-  int	i;
-  int	j;
+  int			i;
+  int			j;
 
+  j = -1;
   i = 0;
-  j = 0;
-  while (dest[i] != 0)
-  i++;
-  while (src[j] != 0)
-    {
-      /* printf("--%d--dest[%d] = %c && src[%d] = %c\n", __LINE__, i, dest[i], j, src[j]); */
-      dest[i] = src[j];
-      if (l == 1)
-	printf("--%d--dest[%d] = %c && src[%d] = %c\n", __LINE__, i, dest[i], j, src[j]);
-      i++;
-      j++;
-    }
-  if (l == 1)
-    printf("--%d--i a la fin de la boucle = %d\n", __LINE__, i);
-  dest[i] = 0;
-  return (dest);
+  while (dest[++j] != 0);
+    while (src[i] != 0)
+    dest[j++] = src[i++];
+  dest[j] = 0;
+  return (0);
 }
 
-char	*my_realloc(char *str, int boucle)
+char			*my_realloc(char *str, int nbr)
 {
-  char	*new;
+  char			*tmp;
+  int			i;
 
-  printf("--%d-- Malloc = %d\n",  __LINE__, READ_SIZE * boucle);
-  /* printf("--%d-- &str = %p\n",  __LINE__, str); */
-  new = malloc(READ_SIZE * boucle + 1);
-  new[0] = 0;
-  if (boucle != 1)
+  i = -1;
+  if ((tmp = malloc(READ_SIZE * nbr + 1)) == NULL)
+    return (NULL);
+  tmp[0] = 0;
+  if (nbr > 1)
     {
-      new = my_strcat(new, str, 0);
+      my_strncat(tmp, str);
       free(str);
     }
-  return (new);
+  tmp[READ_SIZE * nbr] = 0;
+  return (tmp);
 }
 
-char	*check(char *str, char *buffer)
+static char		*check_n(char *str, char* stat)
 {
-  int	i;
-  int	j;
+  int		i;
+  int		j;
 
   j = 0;
   i = 0;
   while (str[i++] != 0)
-    {
-      if (str[i] == 10)
-	while ((str[i] != 0) && (buffer[j++] = str[i]) && (str[i++] = 0));
-    }
-  buffer[j] = 0;
-  printf("--%d");
+    if (str[i] == 10)
+      while (str[i] != 0)
+	{
+	  if (str[i] != 10)
+	    {
+	      stat[j++] = str[i];
+	      str[i] = 0;
+	    }
+	  str[i++] = 0;
+	}
+  stat[j] = 0;
+  return (NULL);
 }
-char	*is_filled(char *str, char *buff)
-{
 
-}
-
-char	*get_next_line(int fd)
+char			*get_next_line(const int fd)
 {
-  static	char	buff[READ_SIZE + 1];
-  char		*str;
-  int		i;
+  static char		buff[READ_SIZE + 1];
+  char			*str;
+  int			i;
+  static int		readed;
 
   i = 1;
   buff[READ_SIZE] = 0;
-  while (read(fd, buff, READ_SIZE) != 0)
+  if ((str = my_realloc(str, i)) == NULL)
+    return (NULL);
+  if (buff[0] != 0 && (i++) && (i++) && my_strncat(str, buff));
+  while ((readed = read(fd, buff, READ_SIZE)) != 0)
     {
-      str = my_realloc(str, i++);
-      printf("--%d--boucle str = %s\n", __LINE__, str);
-      printf("--%d--boucle buff = %s\n", __LINE__, buff);
-      my_strcat(str, buff, 1);
+      if (readed < READ_SIZE && (buff[readed] = 0));
+	my_strncat(str, buff);
+      if ((str = my_realloc(str, ++i)) == NULL)
+	return (NULL);
+      if ((check_n(str, buff) != NULL) || buff[0] != 0)
+      	return (str);
     }
-  check(str, buff);
-  printf("--%d--buff = %s", __LINE__, buff);
-  return (str);
+  if (str[0] != 0)
+    return (str);
+  if (readed == 0 && buff[0] == 0)
+    return (NULL);
 }
